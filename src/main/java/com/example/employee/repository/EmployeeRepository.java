9257e292486df6b482f337378fc9a1a5fc030139
Lucerne <lucerne_liu@foxmail.com>
2018-04-13 00:11:32 +0800

    feat:finish 1st CURD command and its test "should_return_employee_when_input_employee_name"

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -9,7 +9,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-
+    Employee findTopByName(String name);
+    //方法二
+    Employee queryFirstByName(String name);
+    //方法三
+    Employee findFirstByName(String name);
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
