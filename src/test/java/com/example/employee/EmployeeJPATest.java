7a8c316faf4fea9381d2703feaa30a85f01575d1
Lucerne <lucerne_liu@foxmail.com>
2018-04-13 01:56:04 +0800

    refactor:remove unused code and BUILD SUCCESSFUL

--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -37,7 +37,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_employee_when_input_employee_name() throws Exception {
+    public void should_return_employee_when_input_employee_name(){
         //1.查询名字是小红的employee
         String actualName = "xiaohong";
         Employee expectedEmployee = employeeRepository.findTopByName(actualName);
@@ -49,7 +49,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_employee_given_character_in_name_and_salary_large_than() throws Exception {
+    public void should_return_employee_given_character_in_name_and_salary_large_than(){
         //2.找出Employee表中第一个姓名包含`n`字符的雇员所有个人信息
         //此处补充测试：且工资大于6000
         String actualName = "xiaohong";
@@ -61,7 +61,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
+    public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_(){
         //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
         Employee expectedEmployee = employeeRepository.findMaxSalaryByCompanyId(1);
         String actualName = "xiaohong";
@@ -69,7 +69,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_employee_list_when_input_page_request() throws Exception {
+    public void should_return_employee_list_when_input_page_request(){
         //4.实现对Employee的分页查询，每页两条数据，一共三页数。
         //注意：PageRequest的构造方法已经弃用了代替的是PageRequest.of,并且最后一个参数代表按照table中的哪一个字段排序
         Page<Employee> EmployeePage =employeeRepository.findAll(PageRequest.of(3, 2));
@@ -77,7 +77,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_company_name_when_input_employee_name() throws Exception {
+    public void should_return_company_name_when_input_employee_name(){
         //5.查找xiaohong的所在的公司的公司名称
         String expectedCompanyName = "alibaba";
         String actualCompanyName = employeeRepository.findCompanyNameByEmployeeName("xiaohong");
@@ -85,7 +85,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_return_influence_lines_when_update_employee_name() throws Exception {
+    public void should_return_influence_lines_when_update_employee_name(){
         //6.将xiaohong的名字改成xiaobai,输出这次修改影响的行数
         Integer expectedLine = 1;
         Integer actualLine = employeeRepository.modifyByIName("xiaobai","xiaohong");
@@ -93,7 +93,7 @@ public class EmployeeJPATest {
     }
 
     @Test
-    public void should_deleted_employee_when_given_employee_name() throws Exception {
+    public void should_deleted_employee_when_given_employee_name(){
         //7.删除姓名是xiaohong的employee
         employeeRepository.deleteByName("xiaohong");
         Employee actualEmployee = employeeRepository.findTopByName("xiaohong");
